b68031811a48015e2f3e6e9a93c3dac445cce70c
Timo Wacke <timo.wacke@ommax.de>
2023-12-06 12:31:31 +0100

    dynamic about me page

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ onload = async function () {
     var prjid = "656e5133db0037003ed0127d"
 
     var dyns = document.getElementsByClassName("dynamic");
-            for (var i = 0; i < dyns.length; i++) {
-                vid = dyns.item(i).id
-                await dynamicLoad(url, prjid, vid)
-            }
+    console.log(dyns)
+    for (var i = 0; i < dyns.length; i++) {
+        vid = dyns.item(i).id
+        await dynamicLoad(url, prjid, vid)
+    }
 }
 
 async function dynamicLoad(url, prjid, vid) {
@@ -18,25 +19,32 @@ async function dynamicLoad(url, prjid, vid) {
     await fetch(uri)
         .then(async function (response) {
             await response.text().then(function (text) {
-                document.getElementById(vid).innerHTML = text.replace(/\n/g, '<br/>')
+                let element = document.getElementById(vid);
 
-            })
+                // Check if the element has innerHTML property
+                if (element.innerHTML) {
+                    element.innerHTML = text.replace(/\n/g, '<br/>');
+                } else {
+                    // Use textContent for other types of elements
+                    element.textContent = text;
+                }            })
         });
 
-// insert magic mouse .js like effect of circle following mouse
-var cursor = document.getElementById('cursor');
-var cursor = document.getElementById('cursor');
-var delay = 75; // Delay in milliseconds
 
-document.addEventListener('mousemove', function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.opacity = 1;
+        
+    // insert magic mouse .js like effect of circle following mouse
+    var cursor = document.getElementById('cursor');
+    var delay = 75; // Delay in milliseconds
+    if (cursor)
+        document.addEventListener('mousemove', function (e) {
+            var x = e.clientX;
+            var y = e.clientY;
+            cursor.style.opacity = 1;
 
-    setTimeout(function() {
-        cursor.style.left = x + "px";
-        cursor.style.top = y + "px";
-    }, delay);
-});
+            setTimeout(function () {
+                cursor.style.left = x + "px";
+                cursor.style.top = y + "px";
+            }, delay);
+        });
 }
 
